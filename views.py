0833a8e14d6ba42eb40ab93c1141ec4ad4e5f47a
Simo12343 <djsimpson22@outlook.com.au>
2021-09-18 17:33:28 +1000

    Added comments

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", var=3)
+    return render_template("index.html", var=3) #define variables in this line that can be used in html
 
 @views.route("/about/")
 def about():
